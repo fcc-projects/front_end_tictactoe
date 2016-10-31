fe441ffb7c41b348b4c62d224fbfe98fe84503d0
paravangular <paravangular@gmail.com>
2016-10-31 17:26:22 +0000

    update js

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 $( function() {
 
-	var userChoice = "";
-	var compChoice = "";
+	var winningCombinations = [ [1, 2, 3],
+							    [4, 5, 6],
+							    [7, 8, 9],
+							    [1, 4, 7],
+							    [2, 5, 8],
+							    [3, 6, 9],
+							    [1, 5, 9],
+							    [7, 5, 3]]
 
+	var userChoice = "x";
+	var compChoice = "o";
+	var currPlayer = "user";
 	var seletionMade = false;
 	var gameOver = false;
 
@@ -11,24 +20,89 @@ $( function() {
 
 		if (isEmpty(this)) {
 
-			fillField(this, userChoice);
+			fillField(this, currPlayer);
 
-			checkWin();
+			if (!winOrDraw()) {
+				togglePlayer();
+				compMakeMove();
+			}
 
 		}
 	}
 
-	isEmpty(field) {
+	function isEmpty(field) {
 		return !field.hasClass("filled");
 	}
 
-	fillField(field, icon) {
+	function fillField(field, currPlayer) {
 
-		field.addClass(icon);
-
-	}
-
-	checkWin() {
+		if (currPlayer == "user") {
+			field.addClass(userChoice);
+		} else {
+			field.addClass(compChoice);
+		}
 		
+		field.addClass("filled");
+
 	}
-})
+
+	function checkWin() {
+
+		for (var i = 0; i < winningCombinations.length; ++i) {
+			if (($(".game-container:nth-child(" + winningCombinations[0] + ")").hasClass(userChoice) && 
+				$(".game-container:nth-child(" + winningCombinations[1] + ")").hasClass(userChoice) && 
+				$(".game-container:nth-child(" + winningCombinations[2] + ")").hasClass(userChoice)) || 
+				($(".game-container:nth-child(" + winningCombinations[0] + ")").hasClass(compChoice) && 
+				$(".game-container:nth-child(" + winningCombinations[1] + ")").hasClass(compChoice) && 
+				$(".game-container:nth-child(" + winningCombinations[2] + ")").hasClass(compChoice))) {
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	function checkDraw() {
+		return ($(".filled").length == 9);
+	}
+
+	function togglePlayer() {
+		currPlayer == "user" ? currPlayer = "computer" : currPlayer = "user";
+	}
+
+	function winOrDraw() {
+		if (checkWin()) {
+			gameOver = true;
+			resetBoard();
+			// WINNER CURRPLAYER
+			return true;
+
+		} else if (checkDraw()) {
+
+			gameOver = true;
+			resetBoard();
+				// DRAW POP UP
+
+			return true;
+		}
+
+		return false;
+
+	}
+
+
+	function resetBoard() {
+		$(".game-container > div").removeClass("filled");
+		$(".game-container > div").removeClass(userChoice);
+		$(".game-container > div").removeClass(compChoice);
+	}
+
+	function compMakeMove() {
+		// MINIMAX AI;
+
+		if (!winOrDraw()) {
+			togglePlayer();
+		}
+	}
+});
